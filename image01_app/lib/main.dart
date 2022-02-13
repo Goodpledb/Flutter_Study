59b35744674e2d284394aa347fcf707ce7d4fdab
SeongJun Yoon <50254311+Goodpledb@users.noreply.github.com>
2022-02-13 14:49:13 +0900

    Add files via upload

---
+++ image01_app/lib/main.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.red[200],
+      appBar: AppBar(
+        title: const Text('image01'),
+        backgroundColor: Colors.red,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+            Padding(
+              padding: EdgeInsets.all(10.0),
+              child: CircleAvatar(
+                // backgroundColor: Colors.red,
+                backgroundImage: AssetImage('images/pikachu-1.jpg'),
+                radius: 50.0,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(10.0),
+              child: CircleAvatar(
+                backgroundImage: AssetImage('images/pikachu-2.jpg'),
+                radius: 50.0,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(10.0),
+              child: CircleAvatar(
+                backgroundImage: AssetImage('images/pikachu-3.jpg'),
+                radius: 50.0,
+              ),
+            ),                        
+          ],
+        ),
+      ),
+    );
+  }
+}
